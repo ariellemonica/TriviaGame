f8a521f905cc843107fe856a09c2d4f27673d6bc
Monica Nadal <missmonicanadal@gmail.com>
2020-03-14 18:16:14 -0700

    20200314 fixes to total vs incorrect vs correct upon reset and did some styling, nothing fancy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,35 +2,41 @@
 $("#questionsDiv").hide();
 $("#resultsDiv").hide();
 $("#reset").hide();
+$("#timer").hide();
 var interval = 1000;
 var timer
 var counterCorrect = 0;
 var counterIncorrect = 0;
-var timerCount = 10;
-var totalQuestNum = 8; 
+var timerCount = 15;
+var totalQuestNum = 10;
 
 $("#startBtn").on('click', function (event) {
     event.preventDefault() ;
     $("#landing").hide();
         $("#questionsDiv").show();
-
-
+        $("#timer").show();
     timer = setInterval(stopGame, interval);
 })
+
 function stopGame() {
-    if (timerCount > 1) {
-        $("#timer").text("00:00" + timerCount);
+    if (timerCount > -1) {
+        $("#timer").text(timeConverter(timerCount));
         timerCount--;
     } else {
         $("#questionsDiv").hide();
         $("#resultsDiv").show();
         $("#reset").show();
-        //remember to make a results div :)
-        if (counterCorrect + counterIncorrect !== totalQuestNum){
-            counterIncorrect = totalQuestNum - (counterCorrect + counterIncorrect)
-        }
+
+    // if ((counterCorrect + counterIncorrect) !== totalQuestNum) {
+    //         counterIncorrect = totalQuestNum - (counterCorrect + counterIncorrect)
+    //         console.log("New counter incorrect if not equal to total: " + counterIncorrect);
+    //     }
+
+    counterIncorrect = totalQuestNum - counterCorrect;
+
+
         $("#resultsDiv").empty();
-        $("#resultsDiv").prepend("Right Answers: " + counterCorrect);
+        $("#resultsDiv").prepend("Right Answers: " + counterCorrect + "<br>");
         $("#resultsDiv").prepend("Wrong Answers: " + counterIncorrect + "<br>");
         clearInterval(timer);
     }
@@ -40,14 +46,16 @@ function stopGame() {
 $("#reset").on("click", function (event){
     counterCorrect = 0;
     counterIncorrect = 0; 
-    $("#timer").text("00:00")
-    timerCount = 10;
+    $("#timer").text("00:00");
+    $("#resultsDiv").empty();
+    timerCount = 60;
+    totalQuestNum = 10;
     timer = setInterval(stopGame, interval);
     $("#questionsDiv").show();
     $("input[class='correct']").prop("checked", false);
     $("input[class='incorrect']").prop("checked", false);
-    // $(".correct").attr("checked", false);
-    // $(".incorrect").attr("checked", false);
+    // $(".correct").prop("checked", false);
+    // $(".incorrect").prop("checked", false);
 })
 
 
@@ -56,13 +64,49 @@ $(".correct").on('click', function () {
 
 })
 
-$(".incorrect").on('click', function () {
-    counterIncorrect++;
+// $(".incorrect").on('click', function () {
+//     counterIncorrect++;
 
-})
+// })
 
-//questions and answers array - if you want :)
+function timeConverter(t){
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
 
+    if (seconds <10) {
+        seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+        minutes = "00";
+    }
+
+    else if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    return minutes + ":" + seconds;
+}
+
+//if you have time for a challenge - 
+//questions and answers array and create the timer/q&a dynamically
+//make questions an empty div
+//make an array variable in the global scope
+//when adding the array, make sure it's formatted as an array of json objects
+//example:
+//questions =[{ question: "first question", answers: ["1option","2option","3optopn"], answer:"rightnswer"]
+
+//make a global variable = currentQuestion
+//function called displayQuestion
+//so when you start the start button, you make container show and call 
+//displayQuestion function - where it shows currentQuestion and options
+
+//modify the onClick for right and wrong - 
+//have a pointer to question array - questio number is your pointer 
+//after displaying the 1st question, user selects - right or wrong, or the timer ends
+//when the user clicks, make it go to the next next question
+//whether right/wrong, timer runs out - increment to right/wrong counters 
+//when increasing display of current question number, NEVER go above questions array - 
+//at this point, just call results function - and make the results function separate to timer 
 //time runs out - stop running 
 
 
